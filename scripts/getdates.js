4c6a6479415be70b7a888ab727bb91d30f69c692
oseigodfred <ogodfred009@gmail.com>
2026-03-07 18:04:23 +0000

    Assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+/* Get current year */
+
+const year = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = year;
+
+
+/* Display last modified date */
+
+document.getElementById("lastModified").textContent =
+    "Last Modified: " + document.lastModified;
