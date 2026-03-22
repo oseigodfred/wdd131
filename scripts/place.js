4c8e56de0b017fc0ad19e8e2f97cb42ab848d966
oseigodfred <ogodfred009@gmail.com>
2026-03-22 12:23:16 +0000

    week03

--- scripts/place.js
+++ scripts/place.js
@@ -1,39 +1,34 @@
+// Footer
 const year = document.querySelector("#year");
 const lastModified = document.querySelector("#lastModified");
 
 year.textContent = new Date().getFullYear();
-lastModified.textContent = document.lastModified;
+lastModified.textContent = "Last Modified: " + document.lastModified;
 
 
-// static weather values
+// Static weather values
 const temperature = 8;
 const windSpeed = 10;
 
 
-// wind chill function
+// Wind chill function
 function calculateWindChill(temp, speed) {
-    return 13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16);
+    return 13.12
+        + (0.6215 * temp)
+        - (11.37 * Math.pow(speed, 0.16))
+        + (0.3965 * temp * Math.pow(speed, 0.16));
 }
 
 
+// Display wind chill
 const windChillElement = document.querySelector("#windchill");
 
 if (temperature <= 10 && windSpeed > 4.8) {
 
     const windChill = calculateWindChill(temperature, windSpeed);
-
     windChillElement.textContent = windChill.toFixed(1) + "°C";
 
-} else function calculateWindChill(temp, speed) {
-    return 13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16);
-}
-
-const temperature = 8;
-const windSpeed = 10;
-
-if (temperature <= 10 && windSpeed > 4.8) {
-    const windChill = calculateWindChill(temperature, windSpeed);
-    console.log(windChill.toFixed(1) + "°C");
 } else {
-    console.log("N/A");
+
+    windChillElement.textContent = "N/A";
 }
